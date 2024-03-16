56026ac31e05b4b19534b1b0db60420e933aed38
Rafael Solis <23090555alumno.utmetropolitana@gmail.com>
2024-03-16 15:01:45 -0600

    Z

--- obj/Debug/net8.0/Tarea-Euler.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tarea-Euler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea-Euler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837ffbd2482157a346166558c2d7c05dca6c73d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367e4be945aecfc34e55956eb62ca8ca9b30c202")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea-Euler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea-Euler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
